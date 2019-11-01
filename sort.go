0473014f921ff712610b38cbb89e222c00160f53
Andrys Daniel Silalahi <andrysds@gmail.com>
2019-11-01 16:40:00 +0700

    insertion sort comments

--- sort.go
+++ sort.go
@@ -1,6 +1,14 @@
 package algorithm
 
 // InsertionSort (https://www.geeksforgeeks.org/insertion-sort/)
+// Time Complexity: O(n*2)
+// Auxiliary Space: O(1)
+// Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
+// Algorithmic Paradigm: Incremental Approach
+// Sorting In Place: Yes
+// Stable: Yes
+// Online: Yes
+// Uses: Insertion sort is used when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
 func InsertionSort(arr []int) []int {
 	for i := 1; i < len(arr); i++ {
 		tmp := arr[i]
