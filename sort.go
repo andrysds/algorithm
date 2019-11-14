aa228eb4db0f59320ed42a8c4630a99712003137
Andrys Daniel Silalahi <andrysds@gmail.com>
2019-11-14 15:06:04 +0700

    merge sort

--- sort.go
+++ sort.go
@@ -21,3 +21,40 @@ func InsertionSort(arr []int) []int {
 	}
 	return arr
 }
+
+// MergeSort (https://www.geeksforgeeks.org/merge-sort/)
+// Time Complexity: Sorting arrays on different machines. Merge Sort is a recursive algorithm and time complexity can be expressed as following recurrence relation.
+// T(n) = 2T(n/2) + Theta(n)
+// The above recurrence can be solved either using Recurrence Tree method or Master method. It falls in case II of Master Method and solution of the recurrence is Theta(n Logn ).
+// Time complexity of Merge Sort is Theta(n Log n) in all 3 cases (worst, average and best) as merge sort always divides the array into two halves and take linear time to merge two halves.
+// Auxiliary Space: O(n)
+// Algorithmic Paradigm: Divide and Conquer
+// Sorting In Place: No in a typical implementation
+// Stable: Yes
+func MergeSort(arr []int) []int {
+	if len(arr) <= 1 {
+		return arr
+	}
+
+	middle := len(arr) / 2
+	leftArr := MergeSort(arr[:middle])
+	rightArr := MergeSort(arr[middle:])
+
+	result := []int{}
+	var leftIndex, rightIndex int
+	for leftIndex < len(leftArr) && rightIndex < len(rightArr) {
+		if leftArr[leftIndex] <= rightArr[rightIndex] {
+			result = append(result, leftArr[leftIndex])
+			leftIndex++
+		} else {
+			result = append(result, rightArr[rightIndex])
+			rightIndex++
+		}
+	}
+	if leftIndex < len(leftArr) {
+		result = append(result, leftArr[leftIndex:]...)
+	} else {
+		result = append(result, rightArr[rightIndex:]...)
+	}
+	return result
+}
--- sort_test.go
+++ sort_test.go
@@ -2,13 +2,25 @@ package algorithm
 
 import "testing"
 
-func TestSort(t *testing.T) {
+func TestInsertionSort(t *testing.T) {
 	in := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
 	want := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	got := InsertionSort(in)
 	for i, v := range want {
 		if v != got[i] {
 			t.Errorf("InsertionSort(%v) = %v; want %v", in, got, want)
+			break
+		}
+	}
+}
+
+func TestMergeSort(t *testing.T) {
+	in := []int{10, 9, 8, 7, 6, 5, 4, 3, 2, 1}
+	want := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	got := MergeSort(in)
+	for i, v := range want {
+		if v != got[i] {
+			t.Errorf("MergeSort(%v) = %v; want %v", in, got, want)
 			break
 		}
 	}
